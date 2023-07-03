40e1ab901f4b1b0822a04f204f755078daf7de93
Dm1tryKov <93584154+Dm1tryKov@users.noreply.github.com>
2023-07-03 10:00:01 +0300

    Add files via upload

---
+++ FactorialCalculator/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FactorialCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateFactorial(object sender, RoutedEventArgs e)
+        {
+            // Преобразование введенного значения в целое число
+            int n = int.Parse(inputTextBox.Text);
+
+            int factorial = 1;
+            string expression = "1";
+
+            // Вычисление факториала
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+                expression += $" * {i}";
+            }
+
+            outputTextBlock.Text = $"fact={expression}={factorial}";
+        }
+    }
+}
